88543516e894cf3e936cd3f4b07ec987f219180d
Rob O'Doherty <rodoherty1@gmail.com>
2021-05-23 20:49:03 +0100

    Updating setup.sh to python3.9.  Also tidy up.

--- setup.sh
+++ setup.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 echo "Installing ansible, ssh and python3"
-sudo apt install -y ansible openssh-server python3.8
+sudo apt install -y ansible openssh-server python3
 
 echo "Enabling ssh"
 sudo systemctl enable ssh
 
 echo "Creating and installing ssh key"
 printf '\n\n\n' | ssh-keygen -t rsa -b 4096 -C "$USER@localhost"
+
 ssh-copy-id $USER@localhost
 
 echo "Creating /etc/ansible/hosts"
+sudo mkdir -p /etc/ansible
 sudo cp ./hosts /etc/ansible/hosts 
 
 echo "Installing python3"
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.9 1
 
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 2
 
